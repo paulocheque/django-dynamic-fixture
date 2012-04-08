18a1cb926d2cad29b140a68dc1e3600f0fc137c2
Paulo <paulocheque@gmail.com>
2012-04-08 00:04:33 -0300

    [config] updating setup.py url to GitHub

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ install_requires = [
 ]
 
 setup(name='django-dynamic-fixture',
-      url='https://bitbucket.org/paulocheque/django-dynamic-fixture',
+      url='https://github.com/paulocheque/django-dynamic-fixture',
       author="paulocheque",
       author_email='paulocheque@gmail.com',
       keywords='python django testing fixture',
