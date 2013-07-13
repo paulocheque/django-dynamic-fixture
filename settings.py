0c0170a83f05bcdfca3ae7f03b4dde7bfa0ece7d
Valder Gallo <valdergallo@gmail.com>
2013-07-12 23:26:19 -0300

    Fixed coverage

--- settings.py
+++ settings.py
@@ -21,4 +21,17 @@ INSTALLED_APPS = (
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 NOSE_PLUGINS = ['queries.Queries', 'ddf_setup.DDFSetup']
 
+# Tell nose to measure coverage on the 'foo' and 'bar' apps
+NOSE_ARGS = [
+    # '--with-coverage',
+    '--cover-html',
+    '--cover-package=django_dynamic_fixture',
+    '--cover-tests',
+    '--cover-erase',
+    ]
+
+# EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+# EMAIL_FILE_PATH = '/tmp/invest-messages'  # change this to a proper location
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 # python manage.py test --with-coverage --cover-inclusive --cover-html --cover-package=django_dynamic_fixture.* --with-queries --with-ddf-setup
