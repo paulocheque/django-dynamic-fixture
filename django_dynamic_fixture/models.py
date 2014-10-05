d2002841f58e848a4db334f06bef6ab07f07569a
PprOR <vincent.desprez@gmail.com>
2014-10-05 18:37:43 +0200

    bugfix
    Avoid the AttributeError: 'Settings' object has no attribute 'IMPORT_DDF_MODELS' error

--- django_dynamic_fixture/models.py
+++ django_dynamic_fixture/models.py
@@ -2,5 +2,5 @@ from django.conf import settings
 
 import_models = getattr(settings, 'IMPORT_DDF_MODELS', False)
 
-if settings.IMPORT_DDF_MODELS:
+if import_models:
     from django_dynamic_fixture.models_test import *
