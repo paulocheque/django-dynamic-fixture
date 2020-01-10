9660e0e65385391d8b11b0fcb10de6c567fdf2f1
Paulo Cheque <paulocheque@gmail.com>
2020-01-10 09:06:18 -0300

    fix: Using __version__ instead of VERSION

--- ddf/__init__.py
+++ ddf/__init__.py
@@ -1,5 +1,5 @@
 # Short alias to use: # `from ddf import *` instead of `from django_dynamic_fixture import *`
-from django_dynamic_fixture import N, G, F, C, P, PRE_SAVE, POST_SAVE, VERSION
+from django_dynamic_fixture import N, G, F, C, P, PRE_SAVE, POST_SAVE, __version__
 from django_dynamic_fixture import new, get, fixture, teach, look_up_alias
 from django_dynamic_fixture.decorators import skip_for_database, only_for_database
 from django_dynamic_fixture.fdf import FileSystemDjangoTestCase
