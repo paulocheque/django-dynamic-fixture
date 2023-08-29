24eb16af9d21f6e11db3f54d5669f0f92633cf72
Paulo Cheque <paulocheque@gmail.com>
2023-08-29 11:33:15 -0300

    Stop using Travis and start using GitHub actions

--- settings_postgres.py
+++ settings_postgres.py
@@ -1,3 +1,4 @@
+import os
 from settings_ddf import *
 
 DDF_TEST_GEODJANGO = True
@@ -10,11 +11,11 @@ DATABASES = {
         # Postgis supports all Django features
         # createdb ddf
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'ddf',
-        'USER': 'ddf_user', # please, change this if you want to run tests in your machine
-        'PASSWORD': 'ddf_pass',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', 5432),
+        'NAME': os.getenv('POSTGRES_DB', 'ddf'),
+        'USER': os.getenv('POSTGRES_USER', 'ddf_user'), # please, change this if you want to run tests in your machine
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'ddf_pass'),
     }
 }
 
