42d501133a30212cb8d3a7cd2b00af9b2aaaf793
Paulo Cheque <paulocheque@gmail.com>
2023-09-05 17:27:36 -0300

    Forcing RTD theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -102,8 +102,7 @@ pygments_style = 'sphinx'
 html_theme = 'default'
 
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-if not on_rtd:  # only import and set the theme if we're building docs locally
+if (not on_rtd) or True:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
@@ -135,7 +134,7 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
