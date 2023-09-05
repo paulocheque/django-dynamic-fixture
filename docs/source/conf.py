4ae79700d48c1fb307a7437dc256dd099a080a4a
Paulo Cheque <paulocheque@gmail.com>
2023-09-05 17:29:13 -0300

    Fixed Sphinx doc theme (#160)
    * Fixed Sphinx doc theme
    
    * Forcing RTD theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -102,8 +102,7 @@ pygments_style = 'sphinx'
 html_theme = 'default'
 
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-if not on_rtd:  # only import and set the theme if we're building docs locally
+if (not on_rtd) or True:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
